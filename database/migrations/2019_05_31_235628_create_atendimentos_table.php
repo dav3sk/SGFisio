6c2821cfc54d726f682491f66145f9a59dd70067
dav3sk <dav3sk@hotmail.com>
2019-05-31 21:22:06 -0300

    Add migration Atendimento

---
+++ database/migrations/2019_05_31_235628_create_atendimentos_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAtendimentosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('atendimentos', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('guia_id')->unsigned();
+            $table->foreign('guia_id')->references('id')->on('guias');
+            $table->string('CID');
+            $table->enum('tipo_atendimento', [
+                'Orto',
+                'Neuro',
+                'Geronto'
+            ]);
+            $table->integer('quantidade_sessoes');
+            $table->date('inicio');
+            $table->date('fim');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('atendimentos');
+    }
+}
