d630a7688902df3da415cb46e5d093b28d5e083d
dav3sk <dav3sk@hotmail.com>
2019-06-03 15:21:18 -0300

    Configura AdminLTE

--- config/app.php
+++ config/app.php
@@ -175,6 +175,11 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        /*
+        * AdminLTE
+        */
+        JeroenNoten\LaravelAdminLte\ServiceProvider::class,
+
     ],
 
     /*
