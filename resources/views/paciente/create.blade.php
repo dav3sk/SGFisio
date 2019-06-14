60048f13a5a523bb14f1f7e0ac61c10104148f53
dav3sk <dav3sk@hotmail.com>
2019-06-14 18:18:10 -0300

    Add blade de paciente create

---
+++ resources/views/paciente/create.blade.php
@@ -0,0 +1,17 @@
+@extends('adminlte::page')
+
+@section('title', 'Cadastrar novo paciente')
+
+@section('css')
+@endsection
+
+@section('content_header')
+    @include('helpers.flash-message')
+    <h1>Cadastrar novo paciente</h1>
+@stop
+
+@section('content')
+    @include('paciente._form', [
+        'form' => $form
+    ])
+@stop
