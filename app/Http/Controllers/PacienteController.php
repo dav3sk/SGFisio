1bb7977c8d52e7c7b0269d3a699152a8f7a40085
dav3sk <dav3sk@hotmail.com>
2019-06-14 11:15:17 -0300

    Controller de paciente

---
+++ app/Http/Controllers/PacienteController.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Paciente;
+use App\Forms\PacienteForm;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class PacienteController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $pacientes = Paciente::all();
+
+        return $pacientes;
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $form = $this->formBuilder->create(PacienteForm::class, [
+            'url' => route('paciente.store'),
+            'method' => 'POST'
+        ]);
+
+        return view('paciente.create', [
+            'form' => $form
+        ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $paciente = new Paciente($request->all());
+
+        // try {
+            $paciente->save();
+
+        //     return redirect()
+        //         ->route('assisteds.index')
+        //         ->with('alert-success', 'Assistido cadastrado com sucesso!');
+        // } catch (\Exception $e) {
+        //     return redirect()
+        //         ->back()
+        //         ->with('alert-danger', 'Falha no cadastro do assistido!' . $e->getMessage());
+        // }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
