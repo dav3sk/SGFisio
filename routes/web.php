446f763adb52a57ca95d9a757e99b17ba0b00268
dav3sk <dav3sk@hotmail.com>
2019-06-18 18:00:21 -0300

    Corrigido rotas de paciente

--- routes/web.php
+++ routes/web.php
@@ -22,11 +22,11 @@ Route::get('/home', 'HomeController@index')->name('home');
 // Rotas Pacientes
 Route::group(['prefix' => 'pacientes'], function () {
     Route::get('/', 'PacienteController@index')->name('paciente.index');
-    Route::get('/show', 'PacienteController@show')->name('pacientes.show');
+    Route::get('/{paciente}', 'PacienteController@show')->name('pacientes.show');
 
     Route::get('/create', 'PacienteController@create')->name('pacientes.create');
     Route::post('/', 'PacienteController@store')->name('paciente.store');
 
-    Route::get('/destroy', 'PacienteController@destroy')->name('pacientes.destroy');
-    Route::get('/edit', 'PacienteController@edit')->name('pacientes.edit');
+    Route::get('/{paciente/}destroy', 'PacienteController@destroy')->name('pacientes.destroy');
+    Route::get('/{paciente}/edit', 'PacienteController@edit')->name('pacientes.edit');
 });
