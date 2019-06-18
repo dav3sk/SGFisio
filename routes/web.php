6605239399db40b202a26351f96e9c23adaa9a16
dav3sk <dav3sk@hotmail.com>
2019-06-18 16:45:00 -0300

    Add rota de show

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,11 @@ Route::get('/home', 'HomeController@index')->name('home');
 // Rotas Pacientes
 Route::group(['prefix' => 'pacientes'], function () {
     Route::get('/', 'PacienteController@index')->name('paciente.index');
+    Route::get('/{paciente}', 'PacienteController@show')->name('paciente.show');
+
     Route::get('/create', 'PacienteController@create')->name('paciente.create');
     Route::post('/', 'PacienteController@store')->name('paciente.store');
+
+    Route::put('/{paciente}', 'PacienteController@update')->name('paciente.update');
+    Route::get('/{paciente}/edit', 'PacienteController@edit')->name('paciente.edit');
 });
