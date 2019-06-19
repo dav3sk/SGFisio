ff0f2e508dd1abc55ec0d6cae19cfe16fe902730
dav3sk <dav3sk@hotmail.com>
2019-06-19 15:28:05 -0300

    Add factory de Sessao

---
+++ database/factories/SessaoFactory.php
@@ -0,0 +1,17 @@
+<?php
+
+/* @var $factory \Illuminate\Database\Eloquent\Factory */
+
+use App\Models\Sessao;
+use App\Models\Atendimento;
+use App\Models\Plantonista;
+use Faker\Generator as Faker;
+
+$factory->define(Sessao::class, function (Faker $faker) {
+    return [
+        'atendimento_id' => Atendimento::inRandomOrder()->get()->first()->id,
+        'plantonista_id' => Plantonista::inRandomOrder()->get()->first()->id,
+        'data_hora' => $faker->dateTime(),
+        'evolucao' => $faker->text()
+    ];
+});
