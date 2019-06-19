d40885f74ff83d81b11f6d36342f3facf4cf9dca
dav3sk <dav3sk@hotmail.com>
2019-06-19 14:14:28 -0300

    Del comentarios

--- app/Models/Plantonista.php
+++ app/Models/Plantonista.php
@@ -8,9 +8,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class Plantonista extends Model
 {
     use SoftDeletes;
-    /**
-     * Atributos do paciente
-     */
+
     protected $fillable = [
         'nome',
         'email',
