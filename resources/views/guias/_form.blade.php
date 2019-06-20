03137df407c9c030ea235fcb89b93e41c4be6ee2
HigorG <higorgardin@hotmail.com>
2019-06-20 19:12:10 -0300

    Criado instanciação de campos no form das guias

---
+++ resources/views/guias/_form.blade.php
@@ -0,0 +1,39 @@
+{!! form_start($form) !!}
+    <table class="table table-bordered table-condensed">
+        <tbody>
+            <div class="col-md-6">
+                <div class="box box-success" style="border: 1px solid #d2d6de;">
+                    <div class="box-header with-border">
+                        <h3 class="box-title">Informações da Guia</h3>
+                    </div>
+                    
+                    <div class="box-body">
+                        <div class="form-group">
+                            {!! form_label($form->data_emissao) !!}
+                            {!! form_widget($form->data_emissao) !!}
+                        </div>
+
+                        <div class="form-group">
+                            {!! form_label($form->diagnostico) !!}
+                            {!! form_widget($form->diagnostico) !!}
+                        </div>
+
+                        <div class="form-group">
+                            {!! form_label($form->tempo_de_lesao) !!}
+                            {!! form_widget($form->tempo_de_lesao) !!}
+                        </div>
+
+                        <div class="form-group">
+                            {!! form_label($form->prioridade) !!}
+                            {!! form_widget($form->prioridade) !!}
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+            <div class="col-md-12" style="padding-bottom: 10px;">
+                {!! form_widget($form->submit) !!}
+            </div>
+        </tbody>
+    </table>
+{!! form_end($form) !!}
