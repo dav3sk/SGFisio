6c17fdf2d3b650a4b4d0eb70c885f5d6a2f0c7a2
dav3sk <dav3sk@hotmail.com>
2019-06-21 13:50:15 -0300

    Adequado views de plantonista

--- resources/views/plantonistas/show.blade.php
+++ resources/views/plantonistas/show.blade.php
@@ -11,36 +11,45 @@
 @stop
 
 @section('content')
-    <div class="col-md-8 col-md-offset-2">
-        <div class="box box-primary">
-            <div class="box-header with-border">
-                <div class="pull-right">
-                    <a class="btn btn-xs btn-primary" href="{{ route('plantonistas.edit', $plantonista->id) }}">Editar plantonista</a>
+    @include('helpers._button_voltar', [
+        'rota' => 'plantonistas.index'
+    ])
+
+    <table class="table table-bordered table-condensed">
+        <tbody>
+            <div class="col-md-6">
+                <div class="box box-success" style="border: 1px solid #d2d6de;">
+                    <div class="box-header with-border">
+                        <h3 class="box-title">Informações Pessoais</h3>
+                    </div>
+
+                    <div class="box-body">
+                        <p> <b>Nome:</b> {{ $plantonista->nome }} </p>
+                        <p> <b>E-mail:</b> {{ $plantonista->email }} </p>
+                        <p> <b>Sexo:</b> {{ $plantonista->sexo }} </p>
+                        <p> <b>Data de Nascimento:</b> {{ date('d/m/Y', strtotime($plantonista->data_nascimento)) }} </p>
+                        <p> <b>CPF:</b> {{ $plantonista->cpf }} </p>
+                        <p> <b>Crefito:</b> {{ $plantonista->crefito }} </p>
+                        <p> <b>Telefone:</b> {{ $plantonista->telefone }} </p>
+                    </div>
                 </div>
             </div>
-            <!-- /.box-header -->
-            <div class="box-body">
-                <div class="col-md-6">
-                    <h2>Informações Pessoais</h2>
 
-                    <p> <b>Nome:</b> {{ $plantonista->nome }} </p>
-                    <p> <b>E-mail:</b> {{ $plantonista->email }} </p>
-                    <p> <b>Sexo:</b> {{ $plantonista->sexo }} </p>
-                    <p> <b>Data de Nascimento:</b> {{ date('d/m/Y', strtotime($plantonista->data_nascimento)) }} </p>
-                    <p> <b>CPF:</b> {{ $plantonista->cpf }} </p>
-                    <p> <b>Crefito:</b> {{ $plantonista->crefito }} </p>
-                    <p> <b>Telefone:</b> {{ $plantonista->telefone }} </p>
-                </div>
+            <div class="col-md-6">
+                <div class="box box-success" style="border: 1px solid #d2d6de;">
 
-                <div class="col-md-6">
-                    <h2>Endereço</h2>
+                    <div class="box-header with-border">
+                        <h3 class="box-title">Endereço</h3>
+                    </div>
 
-                    <p> <b>Cidade:</b> {{ $plantonista->cidade }} </p>
-                    <p> <b>Estado:</b> {{ $plantonista->estado }} </p>
+                    <div class="box-body">
+                        <p> <b>Cidade:</b> {{ $plantonista->cidade }} </p>
+                        <p> <b>Estado:</b> {{ $plantonista->estado }} </p>
+                    </div>
                 </div>
             </div>
-        </div>
-    </div>
+        </tbody>
+    </table>
 @stop
 
 @section('js')
