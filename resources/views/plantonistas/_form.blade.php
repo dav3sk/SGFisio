1781cad48fbe741e899f77468eb3a403a8640b75
HigorG <higorgardin@hotmail.com>
2019-06-21 13:45:45 -0300

    Padronizado view de form de plantonista

--- resources/views/plantonistas/_form.blade.php
+++ resources/views/plantonistas/_form.blade.php
@@ -1,76 +1,75 @@
 {!! form_start($form) !!}
+    <table class="table table-bordered table-condensed">
+        <tbody>
+            <div class="col-md-6">
+                <div class="box box-success" style="border: 1px solid #d2d6de;">
+                    <div class="box-header with-border">
+                        <h3 class="box-title">Informações Pessoais</h3>
+                    </div>
+                    
+                    <div class="box-body">
+                        <div class="form-group">
+                            {!! form_label($form->nome) !!}
+                            {!! form_widget($form->nome) !!}
+                        </div>
 
-<div class="row col-md-offset-1">
+                        <div class="form-group">
+                            {!! form_label($form->email) !!}
+                            {!! form_widget($form->email) !!}
+                        </div>
 
-    <div class="col-md-5">
-        <div class="box box-primary">
+                        <div class="form-group">
+                            {!! form_label($form->cpf) !!}
+                            {!! form_widget($form->cpf) !!}
+                        </div>
 
-            <div class="box-header with-border">
-                <h3 class="box-title">Informações Pessoais</h3>
-            </div>
+                        <div class="form-group">
+                            {!! form_label($form->crefito) !!}
+                            {!! form_widget($form->crefito) !!}
+                        </div>
 
-            <div class="box-body">
-                <div class="form-group">
-                    {!! form_label($form->nome) !!}
-                    {!! form_widget($form->nome) !!}
-                </div>
+                        <div class="form-group">
+                            {!! form_label($form->data_nascimento) !!}
+                            {!! form_widget($form->data_nascimento) !!}
+                        </div>
 
-                <div class="form-group">
-                    {!! form_label($form->email) !!}
-                    {!! form_widget($form->email) !!}
-                </div>
+                        <div class="form-group">
+                            {!! form_label($form->sexo) !!}
+                            {!! form_widget($form->sexo) !!}
+                        </div>
 
-                <div class="form-group">
-                    {!! form_label($form->cpf) !!}
-                    {!! form_widget($form->cpf) !!}
-                </div>
-
-                <div class="form-group">
-                    {!! form_label($form->crefito) !!}
-                    {!! form_widget($form->crefito) !!}
-                </div>
-
-                <div class="form-group">
-                    {!! form_label($form->data_nascimento) !!}
-                    {!! form_widget($form->data_nascimento) !!}
-                </div>
-
-                <div class="form-group">
-                    {!! form_label($form->sexo) !!}
-                    {!! form_widget($form->sexo) !!}
-                </div>
-
-                <div class="form-group">
-                    {!! form_label($form->telefone) !!}
-                    {!! form_widget($form->telefone) !!}
-                </div>
-            </div>
-        </div>
-    </div>
-
-    <div class="col-md-5">
-        <div class="box box-primary">
-
-            <div class="box-header with-border">
-                <h3 class="box-title">Endereço</h3>
-            </div>
-
-            <div class="box-body">
-                <div class="form-group">
-                    {!! form_label($form->cidade) !!}
-                    {!! form_widget($form->cidade) !!}
-                </div>
-
-                <div class="form-group">
-                    {!! form_label($form->estado) !!}
-                    {!! form_widget($form->estado) !!}
+                        <div class="form-group">
+                            {!! form_label($form->telefone) !!}
+                            {!! form_widget($form->telefone) !!}
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
-    </div>
+    
+            <div class="col-md-6">
+                <div class="box box-success" style="border: 1px solid #d2d6de;">
 
-    <div class="col-md-10">
-        {!! form_widget($form->submit) !!}
-    </div>
-</div>
+                    <div class="box-header with-border">
+                        <h3 class="box-title">Endereço</h3>
+                    </div>
+
+                    <div class="box-body">
+                        <div class="form-group">
+                            {!! form_label($form->cidade) !!}
+                            {!! form_widget($form->cidade) !!}
+                        </div>
+
+                        <div class="form-group">
+                            {!! form_label($form->estado) !!}
+                            {!! form_widget($form->estado) !!}
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-12" style="padding-bottom: 10px;">
+                {!! form_widget($form->submit) !!}
+            </div>
+        </tbody>
+    </table>
 {!! form_end($form) !!}
